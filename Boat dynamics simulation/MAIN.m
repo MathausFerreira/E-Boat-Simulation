f4b993d7dbf195ed9b70a417c8cdfc6fcef56fd6
Mathaus Ferreira da Silva <mathaus.silva@engenharia.ufjf.br>
2021-04-27 13:21:49 -0300

    Saving tha 3 figures in the same image

--- Boat dynamics simulation/MAIN.m
+++ Boat dynamics simulation/MAIN.m
@@ -18,79 +18,75 @@ Plot       = 0;         % 1 - Para plotar figuras durante a simulação / 0 caso c
 Plot_Step  = 20;        % Tempo para plot dinâmico
 Salvar     = 01;        % 1 - Para salvar as figuras plotadas / 0 caso contrário
 
-padrao = [80  0  15/20]
-Param = [1*padrao;
+padrao = [80  0  15/20];
+Param  = [1*padrao;
     10*padrao;
-    20*padrao+[0 0 0]]
+    20*padrao+[0 0 0]];
 
-% Param = [80 0 15;
-%     10*80 0 15;
-%     20*80 0 15]
-V_relative = [-0 0 0]; % Velocidade da correnteza
+V_relat_vec = [6;0;-6];
 
-for h=1:length(Param(:,1))
-    %%
-    Dv_h = zeros(3,1);
-    Cv_h = zeros(3,1);
-    Dv_n = zeros(3,1);
-    Dv_l = zeros(3,1);
+for p = 1:length(V_relat_vec)
+    V_relative = [V_relat_vec(p) 0 0]; % Velocidade da correnteza
     
-    Initialisation;                 % Time and some variables
-    PhysicalProperties2(Param(h,1),Param(h,2),Param(h,3));             % Catamaran Physical Properties
-    Calc_Controllers;               % Controladores de velocidade
-    
-    %% Auxiliar variables
-    Aux = [];
-    
-    %% Inicio da Simulação
-    ImgParam = parametrizacaoFigura;
-    figLA = figure(ImgParam.figOpt{:});
-    CALADO = LinhaDagua;                     % Plota a altura da linha d'agua no casco
-    
-    tic;
-    vc=0;% Inicia o timer da simulação
-    for i = 1:numel(Time)
+    for h=1:length(Param(:,1))
+        %%
+        Dv_h = zeros(3,1);
+        Cv_h = zeros(3,1);
+        Dv_n = zeros(3,1);
+        Dv_l = zeros(3,1);
         
-        Sim.Vel(:,i) = [vc;0;0];
-        vc =vc+50;
-        for j = (SLC.Freq*(i-1)+1):SLC.Freq*(i)
-            %% Controlador de Velocidade
-            Speed_Controller(i,j);
+        Initialisation;                 % Time and some variables
+        PhysicalProperties2(Param(h,1),Param(h,2),Param(h,3));             % Catamaran Physical Properties
+        Calc_Controllers;               % Controladores de velocidade
+        
+        %% Auxiliar variables
+        Aux = [];
+        vel = Sp_poli(0,5,0,50,0,0,Sim.Ts*SLC.Freq,Time(end));
+        
+        %% Inicio da Simulação
+        ImgParam = parametrizacaoFigura;
+        figLA = figure(ImgParam.figOpt{:});
+        CALADO = LinhaDagua;                     % Plota a altura da linha d'agua no casco
+        
+        tic; % Inicia o timer da simulação
+        for i = 1:numel(Time)
             
-            %% Computes the net forces and moments acting on the ROV
-            ROVLoads(j);
-            
-            n_dot = BF2NED(Sim.Current_X_Y_psi(3),Sim.Current_u_v_r);
-            v_dot = ROV.InverseInertia * (Torque - Sim.NetForces);
-            %% Double integration: accelerations -> velocities -> position/attitude
-            X                = [n_dot; v_dot];             % State vector
-            [AuxVector, Aux] = Integration(X,Aux,j);
-            
-            Sim.u_v_r_dot(:,j+1) = v_dot;         % utilizada no controle de Velocidade
-            Sim.Current_X_Y_psi  = AuxVector(1:3);% + sqrt(R_gps)*randn(size(AuxVector(1:3)));
-            Sim.Current_u_v_r    = AuxVector(4:6);% + sqrt(R_gps)*randn(size(AuxVector(1:3)));
-            
-            %% Armazena para PLOT
-            Sim_Plot.u_v_r(:,j)   = Sim.Current_u_v_r;
-            Sim_Plot.X_Y_psi(:,j) = Sim.Current_X_Y_psi;
+            Sim.Vel(:,i) = [vel(i);0;0];
+            for j = (SLC.Freq*(i-1)+1):SLC.Freq*(i)
+                %% Controlador de Velocidade
+                Speed_Controller(i,j);
+                
+                %% Computes the net forces and moments acting on the ROV
+                ROVLoads(j);
+                
+                n_dot = BF2NED(Sim.Current_X_Y_psi(3),Sim.Current_u_v_r);
+                v_dot = ROV.InverseInertia * (Torque - Sim.NetForces);
+                %% Double integration: accelerations -> velocities -> position/attitude
+                X                = [n_dot; v_dot];             % State vector
+                [AuxVector, Aux] = Integration(X,Aux,j);
+                
+                Sim.u_v_r_dot(:,j+1) = v_dot;         % utilizada no controle de Velocidade
+                Sim.Current_X_Y_psi  = AuxVector(1:3);% + sqrt(R_gps)*randn(size(AuxVector(1:3)));
+                Sim.Current_u_v_r    = AuxVector(4:6);% + sqrt(R_gps)*randn(size(AuxVector(1:3)));
+                
+                %% Armazena para PLOT
+                Sim_Plot.u_v_r(:,j)   = Sim.Current_u_v_r;
+                Sim_Plot.X_Y_psi(:,j) = Sim.Current_X_Y_psi;
+                
+            end
+            % Plot online Figure
+            if(mod(i,Plot_Step)==0 && Plot==1)
+                PlotBarco();
+                pause(1)
+            end
             
         end
-        % Plot online Figure
-        if(mod(i,Plot_Step)==0 && Plot==1)
-            PlotBarco();
-            pause(1)
-        end
+        %% Stops and reads the stopwatch timer
+        elapsedTime = toc;
+        fprintf('Total simulation time = %0.4fs\n', elapsedTime);
         
-    end
-    %% Stops and reads the stopwatch timer
-    elapsedTime = toc;
-    fprintf('Total simulation time = %0.4fs\n', elapsedTime);
-    
-    plotFinal;
-    
-    
-    if Salvar
-        saveas(figLA,strcat(strcat('LinhaDagua30cm',CALADO),'.png'))
+        plotFinal(CALADO,Salvar);
+        
+        
     end
 end
-
--- Boat dynamics simulation/plotFinal.m
+++ Boat dynamics simulation/plotFinal.m
@@ -1,93 +1,136 @@
-function plotFinal()
+function plotFinal(CALADO,Salvar)
 
 ImgParam = parametrizacaoFigura;
 
-global TimeJ Sim_Plot Sim Dv_h Cv_h Dv_n Dv_l
+global TimeJ Sim_Plot Sim Dv_h Cv_h Dv_n Dv_l ROV V_relative
 
-%% VELOCIDADE
-figVel = figure(ImgParam.figOpt1L{:});
-% ax1    = subplot(311);
+% % % %% POSIÇÃO
+% % % figPos = figure(ImgParam.figOpt1L{:});
+% % % % ax1    = subplot(311);
+% % % plot(TimeJ,Sim_Plot.X_Y_psi(1,:),'linewidth',2); grid on;
+% % % xlabel('Tempo (s)',ImgParam.Label{:});
+% % % ylabel('Dist\^{a}ncia (m)',ImgParam.Label{:});
+% % % legend('Posi\c{c}\~{a}o',ImgParam.Legend{:});
+% % % 
+% % % % ax2 = subplot(312)
+% % % % plot(TimeJ,Sim_Plot.X_Y_psi(2,:),'linewidth',2); grid on;
+% % % % xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
+% % % % ylabel('Dist\^{a}ncia (m)',ImgParam.Label{:});
+% % % %
+% % % % ax3 = subplot(313)
+% % % % plot(TimeJ,Sim_Plot.X_Y_psi(3,:),'linewidth',2); grid on;
+% % % % xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
+% % % % ylabel('Angulo (graus)',ImgParam.Label{:});
+% % % %
+% % % % linkaxes([ax1 ax2 ax3],'xy')
+% % % 
+% % % %% VELOCIDADE
+% % % figVel = figure(ImgParam.figOpt1L{:});
+% % % % ax1    = subplot(311);
+% % % plot(TimeJ,Sim_Plot.u_v_r(1,:),'linewidth',2); grid on;
+% % % xlabel('Tempo (s)',ImgParam.Label{:});
+% % % ylabel('Velocidade (m/s)',ImgParam.Label{:});
+% % % legend('Velocidade Linear',ImgParam.Legend{:});
+% % % %
+% % % % ax2 = subplot(312)
+% % % % plot(TimeJ,Sim_Plot.u_v_r(2,:),'linewidth',2); grid on;
+% % % % xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
+% % % % ylabel('Velocidade (m/s)',ImgParam.Label{:});
+% % % %
+% % % % ax3 = subplot(313)
+% % % % plot(TimeJ,Sim_Plot.u_v_r(3,:),'linewidth',2); grid on;
+% % % % xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
+% % % % ylabel('Velocidade Angular (rad/s)',ImgParam.Label{:});
+% % % %
+% % % % linkaxes([ax1 ax2 ax3],'x')
+% % % 
+% % % %% FORÇA
+% % % figForca = figure(ImgParam.figOpt1L{:});
+% % % % ax1    = subplot(311);
+% % % plot(TimeJ,Sim.F(1,:),'linewidth',2); grid on;
+% % % xlabel('Tempo (s)',ImgParam.Label{:});
+% % % ylabel('Forca (N)',ImgParam.Label{:});
+% % % legend('Motores',ImgParam.Legend{:});
+% % % % title('Posi\c{c}\~{a}o',ImgParam.Label{:});
+% % % 
+% % % % ax2 = subplot(312)
+% % % % plot(TimeJ,Sim.F(2,:),'linewidth',2); grid on;
+% % % % xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
+% % % % ylabel('Forca (N)',ImgParam.Label{:});
+% % % %
+% % % % ax3 = subplot(313)
+% % % % plot(TimeJ,Sim.F(3,:),'linewidth',2); grid on;
+% % % % xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
+% % % % ylabel('Forca',ImgParam.Label{:});
+% % % 
+% % % % linkaxes([ax1 ax2 ax3],'x')
+% % % %% HIDRODINAMICA
+% % % fighidro = figure(ImgParam.figOpt1L{:});
+% % % % ax1    = subplot(211);
+% % % 
+% % % Dv_h = Dv_h(:,2:end);
+% % % Dv_n = Dv_n(:,2:end);
+% % % Dv_l = Dv_l(:,2:end);
+% % % Cv_h = Cv_h(:,2:end);
+% % % 
+% % % plot(TimeJ,Dv_h(1,:),'--b','linewidth',2);hold on;grid on
+% % % plot(TimeJ,Dv_n(1,:),'-.m','linewidth',2);
+% % % plot(TimeJ,Dv_l(1,:),'-.r','linewidth',2);
+% % % xlabel('Tempo (s)',ImgParam.Label{:});
+% % % ylabel('Resistencia Hidrodinamica (kg.m/s)',ImgParam.Label{:});
+% % % legend('Amortecimento Hidrodinamico','Amortecimento N\~{a}o Linear',...
+% % %     'Amortecimento Linear',ImgParam.Legend{:})
+% % % 
+% % % % ax3    = subplot(313);
+% % % % plot(TimeJ,Dv_h(3,:),'--b','linewidth',2);hold on;grid on
+% % % % % plot(TimeJ,Cv_h(3,:),'-.m','linewidth',2);
+% % % % xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
+% % % % ylabel('Hidro z',ImgParam.Label{:});
+% % % %
+% % % % linkaxes([ax1 ax2 ax3],'x')
+%% TODAS
+figALL = figure(ImgParam.figOpt3L{:});
+ax1 =subplot(311);
 plot(TimeJ,Sim_Plot.u_v_r(1,:),'linewidth',2); grid on;
 xlabel('Tempo (s)',ImgParam.Label{:});
 ylabel('Velocidade (m/s)',ImgParam.Label{:});
-legend('Velocidade Linear',ImgParam.Legend{:});
-% 
-% ax2 = subplot(312)
-% plot(TimeJ,Sim_Plot.u_v_r(2,:),'linewidth',2); grid on;
-% xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
-% ylabel('Velocidade (m/s)',ImgParam.Label{:});
-% 
-% ax3 = subplot(313)
-% plot(TimeJ,Sim_Plot.u_v_r(3,:),'linewidth',2); grid on;
-% xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
-% ylabel('Velocidade Angular (rad/s)',ImgParam.Label{:});
-% 
-% linkaxes([ax1 ax2 ax3],'x')
+legend('Velocidade X',ImgParam.Legend{:});
 
-%% POSIÇÃO
-figPos = figure(ImgParam.figOpt1L{:});
-% ax1    = subplot(311);
-plot(TimeJ,Sim_Plot.X_Y_psi(1,:),'linewidth',2); grid on;
-xlabel('Tempo (s)',ImgParam.Label{:});
-ylabel('Dist\^{a}ncia (m)',ImgParam.Label{:});
-legend('Posi\c{c}\~{a}o',ImgParam.Legend{:});
-
-% ax2 = subplot(312)
-% plot(TimeJ,Sim_Plot.X_Y_psi(2,:),'linewidth',2); grid on;
-% xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
-% ylabel('Dist\^{a}ncia (m)',ImgParam.Label{:});
-% 
-% ax3 = subplot(313)
-% plot(TimeJ,Sim_Plot.X_Y_psi(3,:),'linewidth',2); grid on;
-% xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
-% ylabel('Angulo (graus)',ImgParam.Label{:});
-% 
-% linkaxes([ax1 ax2 ax3],'xy')
-
-%% FORÇA
-figForca = figure(ImgParam.figOpt1L{:});
-% ax1    = subplot(311);
+ax2 =subplot(312);
 plot(TimeJ,Sim.F(1,:),'linewidth',2); grid on;
 xlabel('Tempo (s)',ImgParam.Label{:});
 ylabel('Forca (N)',ImgParam.Label{:});
 legend('Motores',ImgParam.Legend{:});
-% title('Posi\c{c}\~{a}o',ImgParam.Label{:});
 
-% ax2 = subplot(312)
-% plot(TimeJ,Sim.F(2,:),'linewidth',2); grid on;
-% xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
-% ylabel('Forca (N)',ImgParam.Label{:});
-% 
-% ax3 = subplot(313)
-% plot(TimeJ,Sim.F(3,:),'linewidth',2); grid on;
-% xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
-% ylabel('Forca',ImgParam.Label{:});
-
-% linkaxes([ax1 ax2 ax3],'x')
-%% HIDRODINAMICA
-fighidro = figure(ImgParam.figOpt1L{:});
-% ax1    = subplot(211);
-
+ax3 =subplot(313);
 Dv_h = Dv_h(:,2:end);
 Dv_n = Dv_n(:,2:end);
 Dv_l = Dv_l(:,2:end);
 Cv_h = Cv_h(:,2:end);
 
-plot(TimeJ,Dv_h(1,:),'--b','linewidth',2);hold on;grid on
+plot(TimeJ,Dv_h(1,:),'-b','linewidth',2);hold on;grid on
 plot(TimeJ,Dv_n(1,:),'-.m','linewidth',2);
 plot(TimeJ,Dv_l(1,:),'-.r','linewidth',2);
 xlabel('Tempo (s)',ImgParam.Label{:});
-ylabel('Resistencia Hidrodinamica (kg.m/s)',ImgParam.Label{:});
-legend('Amortecimento Hidrodinamico','Amortecimento N\~{a}o Linear',...
-    'Amortecimento Linear',ImgParam.Legend{:})
-% 
+ylabel('Amortecimento (kg.m/s)',ImgParam.Label{:});
+legend('Total','N\~{a}o Linear',...
+    'Linear',ImgParam.Legend{:},'Location','best','NumColumns',3)
 
-% 
-% ax3    = subplot(313);
-% plot(TimeJ,Dv_h(3,:),'--b','linewidth',2);hold on;grid on
-% % plot(TimeJ,Cv_h(3,:),'-.m','linewidth',2);
-% xlabel('Tempo (s)',ImgParam.Label{:},'linewidth',2);
-% ylabel('Hidro z',ImgParam.Label{:});
-% 
-% linkaxes([ax1 ax2 ax3],'x')
+linkaxes([ax1 ax2 ax3],'x');
+
+if Salvar
+    ax1 = strcat('Figs/Vrelat_',string(V_relative(1)));
+    ax2 = strcat(ax1,'_Calado_');
+    ax3 = strcat(ax2,string(CALADO));
+    ax4 = strcat(ax3,'_carga_');
+    ax5 = strcat(ax4,string(ROV.mass));
+    ax6 = strcat(ax5,'_Dist_');
+    ax7 = strcat(ax6,string(Sim_Plot.X_Y_psi(1,end)));
+    
+    saveas(figALL,strcat(strcat(ax7,'_ALL'),'.png'))
+    %     saveas(figVel,strcat(strcat(ax7,'_Vel'),'.png'))
+    %     saveas(figForca,strcat(strcat(ax7,'_Force'),'.png'))
+    %     saveas(fighidro,strcat(strcat(ax7,'_hidro'),'.png'))
+end
+
 end
